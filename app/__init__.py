76f257156a568e26e01d0a494cb927880fb86382
Apoorva Garg <apoorv20@gmail.com>
2018-11-17 17:24:35 -0500

    Fix

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,24 @@
 import os
-from flask import Flask
+from flask import Flask, request
 
 # Initialize application
 app = Flask(__name__)
 
+
+accountSid = "AC295f2f41a6f1ba5e8c8450536b773693"
+authToken = "7a5edf40bd20ed732fb664594ef5a31a"
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/incoming', methods=['POST'])
+def incoming():
+    app.logger.info('Request values %s', request.values)
+    app.logger.info('Request data %s', request.data)
+    app.logger.info('Request headers %s', request.headers)
+    print ('Request values %s', request.values)
+    print('Request data %s', request.data)
+    print('Request headers %s', request.headers)
+    return "All good!"
