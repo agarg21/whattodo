ac94b31717fb6d5368b9d1f26507ca0db05cdf6b
Apoorva Garg <apoorv20@gmail.com>
2018-11-17 23:04:44 -0500

    Fix

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,30 @@
 import os
 from flask import Flask, request
+from twilio.rest import Client
+from twilio.twiml.messaging_response import Message, MessagingResponse
 
 # Initialize application
 app = Flask(__name__)
 
 
-accountSid = "AC295f2f41a6f1ba5e8c8450536b773693"
-authToken = "7a5edf40bd20ed732fb664594ef5a31a"
+account_sid = "AC295f2f41a6f1ba5e8c8450536b773693"
+auth_token = "7a5edf40bd20ed732fb664594ef5a31a"
+
+
+client = Client(account_sid, auth_token)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-
 @app.route('/incoming', methods=['POST'])
 def incoming():
-    app.logger.info('Request values %s', request.values)
-    app.logger.info('Request data %s', request.data)
-    app.logger.info('Request headers %s', request.headers)
-    print ('Request values %s', request.values)
-    print('Request data %s', request.data)
-    print('Request headers %s', request.headers)
-    return "All good!"
+    app.logger.info('Request form %s', request.form)
+    print ('Request form %s', request.form)
+
+    message_body = request.form['Body']
+    resp = MessagingResponse()
+
+    reply_text = message_body
+    resp.message('Hi\n\n' + reply_text)
+    return str(resp)
